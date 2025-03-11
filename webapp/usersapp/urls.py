e89640991af661aa74f7c569f901e8dc585a3150
Giposfila <ozipov26@yandex.ru>
2025-03-11 12:51:11 +0700

    Добавил стиль к профилю и недостающие ссылки

--- webapp/usersapp/urls.py
+++ webapp/usersapp/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('registration/', views.RegForm_Func, name='regform'),
     path('registration/login/', views.login_view, name='login'),
     path('registration/login/forgotpassword/', views.forgot_password_view, name='forgotpassword'),
+    path('registration/main/', views.MainMenu, name='main'),
     path('registration/login/main/', views.MainMenu, name='main'),
+    path('registration/main/profile/', views.Profile_view, name='profile'),
     path('registration/login/main/profile/', views.Profile_view, name='profile')
 ]
