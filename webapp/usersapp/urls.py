7502d09887fc833ee26295e6e79ad9248f35ea4b
Giposfila <ozipov26@yandex.ru>
2025-03-25 21:23:22 +0700

    адреса исправлены(почти)

--- webapp/usersapp/urls.py
+++ webapp/usersapp/urls.py
@@ -3,8 +3,8 @@ from  . import views
 urlpatterns = [
     path('', views.BlankFunc),
     path('registration/', views.RegForm_Func, name='regform'),
-    path('registration/login/', views.login_view, name='login'),
-    path('registration/login/forgotpassword/', views.forgot_password_view, name='forgotpassword'),
+    path('login/', views.login_view, name='login'),
+    path('forgotpassword/', views.forgot_password_view, name='forgotpassword'),
     path('registration/main/', include('boardsapp.urls')),
     path('registration/login/main/', include('boardsapp.urls')),
 ]
