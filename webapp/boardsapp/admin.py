c448281b20a2bd2c5910ca461eb374e91da17dd9
Giposfila <ozipov26@yandex.ru>
2025-04-21 18:57:06 +0700

    модели комментов и файлов

--- webapp/boardsapp/admin.py
+++ webapp/boardsapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Board, Task
+from .models import Board, Task, Attachment, Comment
 
 admin.site.register(Board)
 admin.site.register(Task)
+admin.site.register(Attachment)
+admin.site.register(Comment)
